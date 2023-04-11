76a135812d4e55742bc98126d59c68d4c53a9205
NomiaPaimoN <112900074+NomiaPaimoN@users.noreply.github.com>
2023-04-11 09:30:29 +0300

    jdjeh

--- лаба 2 2 сем.cpp
+++ лаба 2 2 сем.cpp
@@ -1,109 +1,85 @@
-﻿#include <iostream>
-
+#include <iostream>
 using namespace std;
 
-// структура узла списка
 struct Node {
     int data;
     Node* next;
 };
 
-// структура стека
-struct Stack {
-    Node* top;
-    int size;
-};
-
-// инициализация стека
-void init(Stack& s) {
-    s.top = NULL;
-    s.size = 0;
+void push(Node** head, int data) {
+    Node* node = new Node;
+    node->data = data;
+    node->next = *head;
+    *head = node;
 }
 
-// добавление элемента в стек
-void push(Stack& s, int data) {
-    Node* newNode = new Node;
-    newNode->data = data;
-    newNode->next = s.top;
-    s.top = newNode;
-    s.size++;
-}
-
-// удаление элемента из стека
-void pop(Stack& s) {
-    if (s.top == NULL) {
-        cout << "Стек пуст!" << endl;
-        return;
+void pushBack(Node** head, int data) {
+    Node* node = new Node;
+    node->data = data;
+    node->next = NULL;
+    if (*head == NULL) {
+        *head = node;
+    } else {
+        Node* current = *head;
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        current->next = node;
     }
-    Node* temp = s.top;
-    s.top = s.top->next;
-    delete temp;
-    s.size--;
 }
 
-// получение верхнего элемента стека
-int top(Stack& s) {
-    if (s.top == NULL) {
-        cout << "Стек пуст!" << endl;
-        return -1;
-    }
-    return s.top->data;
-}
-
-// получение минимального значения в стеке
-int getMin(Stack& s) {
-    if (s.top == NULL) {
-        cout << "Стек пуст!" << endl;
-        return -1;
-    }
-    int minVal = s.top->data;
-    Node* current = s.top->next;
+void printStack(Node* head) {
+    Node* current = head;
     while (current != NULL) {
-        if (current->data < minVal) {
-            minVal = current->data;
+        cout << current->data << " ";
+        current = current->next;
+    }
+    cout << endl;
+}
+
+Node* findMin(Node* head) {
+    Node* current = head;
+    Node* minNode = head;
+    while (current != NULL) {
+        if (current->data < minNode->data) {
+            minNode = current;
         }
         current = current->next;
     }
-    return minVal;
+    return minNode;
 }
 
-// перенос элементов из стека в новый стек
-void moveElements(Stack& s, Stack& newStack, int minValue) {
-    while (top(s) != minValue) {
-        push(newStack, top(s));
-        pop(s);
+Node* copyStack(Node* head, Node* minNode) {
+    Node* newHead = NULL;
+    Node* current = head;
+    while (current != minNode) {
+        pushBack(&newHead, current->data);
+        current = current->next;
     }
-    // удаляем элемент с минимальным значением
-    pop(s);
+    return newHead;
 }
 
 int main() {
-    setlocale(LC_ALL, "RU");
-    Stack s;
-    init(s);
+    Node* head = NULL;
 
-    cout << "Введите элементы стека (для окончания ввода введите -1):" << endl;
-    int input;
-    cin >> input;
-    while (input != -1) {
-        push(s, input);
-        cin >> input;
+    cout << "Enter stack values (enter -1 to stop): " << endl;
+    int value;
+    while (true) {
+        cin >> value;
+        if (value == -1) {
+            break;
+        }
+        push(&head, value);
     }
 
-    int minValue = getMin(s);
-    cout << "Минимальное значение в стеке: " << minValue << endl;
+    Node* minNode = findMin(head);
+    Node* newHead = copyStack(head, minNode);
 
-    Stack newStack;
-    init(newStack);
+    cout << "Original stack: ";
+    printStack(head);
 
-    moveElements(s, newStack, minValue);
-
-    cout << "Элементы, перенесенные в новый стек:" << endl;
-    while (newStack.top != NULL) {
-        cout << top(newStack) << " ";
-        pop(newStack);
-    }
-    cout << endl;
+    cout << "New stack: ";
+    printStack(newHead);
 
     return 0;
 }
